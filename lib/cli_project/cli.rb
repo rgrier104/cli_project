dc2a412a409a6a73da442a935c59781932848a62
Rachel Grier <rgrier104@gmail.com>
2018-12-02 23:20:51 +0000

    Updated scraper and CLI and have a working app

--- lib/cli_project/cli.rb
+++ lib/cli_project/cli.rb
@@ -1,8 +1,8 @@
 class BestWine::CLI
 
   def call
-    BestWine::Scraper.new.scrape_wines
-    puts "Welcome to the ultimate wine app"
+    BestWine::Scraper.scrape_wines
+    puts "Welcome to the ultimate wine app. Here are the top 20 wines of 2017:"
     list_wine
     menu
   end
@@ -15,11 +15,19 @@ class BestWine::CLI
   end
 
   def menu
-    puts "Enter the number you would like to learn more about."
+    puts "Enter the number of the wine you would like to learn more about."
     input = gets.strip
     if input.to_i > 0 and input.to_i < 21
       chosen_wine = @wines[input.to_i-1]
-      puts "#{chosen_wine.description}"
+      puts "#{chosen_wine.name}: #{chosen_wine.description}"
+      puts "Would you like to learn about another wine? (y/n)"
+      input2 = gets.strip.downcase
+        if input2 == "y"
+          list_wine
+          menu
+        else
+          exit
+        end
     else
       puts "You entered an invalid number. Please enter a valid number between 1 and 20 or exit."
       menu
